3c149f6f0c56a7313aa554e2383bf3bc551d9cdf
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2021-11-26 18:50:22 +0000

    Added header

---
+++ svelte-files/src/components/mainPageComponents/sections/Header.svelte
@@ -0,0 +1,26 @@
+<header class="bg-white shadow border-t-4 border-green-400 sticky top-0">
+  <div class="container mx-auto px-6 py-4">
+    <div class="flex items-center justify-between">
+      <div>
+        <a
+          href="/"
+          class="flex items-center text-gray-800 hover:text-green-500"
+        >
+          <span class="mx-3 font-medium text-sm md:text-base">
+            Emmanuil B.
+          </span>
+        </a>
+      </div>
+      <div class="flex items-center">
+        <a
+          href="/"
+          class="flex items-center mx-2 text-gray-800 hover:text-green-500">G</a
+        >
+        <a
+          href="/"
+          class="flex items-center mx-2 text-gray-800 hover:text-green-500">L</a
+        >
+      </div>
+    </div>
+  </div>
+</header>
