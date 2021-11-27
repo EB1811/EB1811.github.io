4b642129930b1ba6fddf213bfca22846edff7091
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2021-11-27 18:18:31 +0000

    Added projects section

---
+++ svelte-files/src/components/mainPageComponents/sections/Projects.svelte
@@ -0,0 +1,14 @@
+<section class="bg-gray-700 py-20">
+  <div class="max-w-5xl px-6 mx-auto text-center">
+    <h2 class="text-2xl font-semibold text-white">Projects</h2>
+
+    <div class="flex items-center justify-center mt-12">
+      <a
+        href="/"
+        class="flex items-center text-white hover:underline hover:text-gray-200"
+      >
+        <span>View More On Github</span>
+      </a>
+    </div>
+  </div>
+</section>
