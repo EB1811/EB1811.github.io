dcc015fb1a7d85df7587587999a0a1ab583bcedc
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2021-11-27 17:44:59 +0000

    Added mid section.

---
+++ svelte-files/src/components/mainPageComponents/sections/MidAboutMeSection.svelte
@@ -0,0 +1,14 @@
+<section class="bg-gray-700 py-20">
+  <div class="max-w-5xl px-6 mx-auto text-center">
+    <h2 class="text-2xl font-semibold text-white">
+      About Me or something else
+    </h2>
+    <p class="text-gray-400 mt-4">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ullamcorper nulla
+      nunc quis molestie volutpat elementum at. Ultrices ipsum, enim cursus
+      lorem ac. Orci maecenas praesent arcu eget orci est orci nullam. Leo purus
+      est pellentesque massa at tortor, est. Aliquet pulvinar a mattis sagittis.
+      Suspendisse porta id elementum, massa.
+    </p>
+  </div>
+</section>
