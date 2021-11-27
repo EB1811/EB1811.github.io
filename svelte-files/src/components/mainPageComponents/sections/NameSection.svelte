e7c62502ed9285ba0b3beacf03d087a82c99b0ab
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2021-11-27 17:45:11 +0000

    Added name section.

---
+++ svelte-files/src/components/mainPageComponents/sections/NameSection.svelte
@@ -0,0 +1,11 @@
+<section class="bg-white mt-20" style="min-height: 380px">
+  <div class="max-w-2xl px-6 text-center mx-auto">
+    <h2 class="text-3xl font-semibold text-gray-800">Emmanuils Borovikovs</h2>
+    <p class="text-gray-600 mt-4">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit
+    </p>
+    <div class="flex items-end justify-center mt-16">
+      <svg width="200" height="200" />
+    </div>
+  </div>
+</section>
