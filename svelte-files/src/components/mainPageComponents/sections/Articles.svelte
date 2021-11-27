5c41ca16c7183090f2e87ccc60b11ee3df1728be
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2021-11-27 17:45:24 +0000

    Added articles section

---
+++ svelte-files/src/components/mainPageComponents/sections/Articles.svelte
@@ -0,0 +1,14 @@
+<section class="bg-white py-20">
+  <div class="max-w-5xl px-6 mx-auto text-center">
+    <h2 class="text-2xl font-semibold text-gray-800">Latest Articles</h2>
+    <div class="flex flex-col items-center justify-center mt-6">h1</div>
+    <div class="flex items-center justify-center mt-12">
+      <a
+        href="/"
+        class="flex items-center mx-2 text-gray-600 hover:underline hover:text-gray-500"
+      >
+        <span>View More</span>
+      </a>
+    </div>
+  </div>
+</section>
