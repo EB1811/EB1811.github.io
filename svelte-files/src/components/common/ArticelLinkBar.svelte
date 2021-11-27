21433f65d10c8330766025e1b6fe04dd0fb8f671
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2021-11-27 19:25:46 +0000

    Added article links with custom bar component.

---
+++ svelte-files/src/components/common/ArticelLinkBar.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let title: string
+  export let link: string
+  export let date: string
+  export let first: boolean = false
+</script>
+
+<a
+  class="max-w-2xl w-full block bg-white shadow-md rounded-md border-t-4 border-green-400 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
+  class:mt-8={!first}
+  href={link}
+>
+  <div class="flex items-center justify-between px-4 py-2">
+    <h3 class="text-lg font-medium text-gray-700">{title}</h3>
+    <span class="block text-gray-600 font-light text-sm">{date}</span>
+  </div>
+</a>
--- svelte-files/src/components/mainPageComponents/sections/Articles.svelte
+++ svelte-files/src/components/mainPageComponents/sections/Articles.svelte
@@ -1,7 +1,23 @@
+<script lang="ts">
+  import ArticelLinkBar from '../../common/ArticelLinkBar.svelte'
+</script>
+
 <section class="bg-white py-20">
   <div class="max-w-5xl px-6 mx-auto text-center">
     <h2 class="text-2xl font-semibold text-gray-800">Latest Articles</h2>
-    <div class="flex flex-col items-center justify-center mt-6">h1</div>
+    <div class="flex flex-col items-center justify-center mt-6">
+      <ArticelLinkBar
+        title="Writing cleaner JavaScript with ES6+"
+        date="Coming Soon"
+        link="/"
+        first={true}
+      />
+      <ArticelLinkBar
+        title="Stop Overcomplicating your State – Try Zustand"
+        date="Oct 30 2021"
+        link="https://dev.to/emmanuilsb/stop-overcomplicating-your-state-try-zustand-39p4"
+      />
+    </div>
     <div class="flex items-center justify-center mt-12">
       <a
         href="/"
