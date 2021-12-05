e3786babb4fecf961830dcc88b4012b2e0fa101a
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2021-12-05 16:55:57 +0000

    Name section dark mode.

--- svelte-files/src/components/mainPageComponents/sections/NameSection.svelte
+++ svelte-files/src/components/mainPageComponents/sections/NameSection.svelte
@@ -1,11 +1,15 @@
-<section class="bg-white mt-20" style="min-height: 380px">
+<section
+  class="bg-white mt-20 dark:bg-gray-800 transition-colors duration-500"
+  style="min-height: 380px"
+>
   <div class="max-w-2xl px-6 text-center mx-auto">
     <h2 class="text-3xl font-semibold text-gray-800">
-      <span class="bg-green-400 text-white rounded px-1"
+      <span
+        class="bg-green-400 dark:bg-indigo-700 text-white rounded px-1 duration-700"
         >Emmanuil Borovikovs</span
       >
     </h2>
-    <p class="text-gray-600 mt-4">
+    <p class="text-gray-600 dark:text-gray-300 mt-4">
       Lorem ipsum dolor, sit amet consectetur adipisicing elit
     </p>
     <div class="flex items-end justify-center mt-16">
