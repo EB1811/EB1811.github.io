ecbf764f41ce0334e49a7d5b2211bd0eb23ec567
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2023-10-23 12:24:07 +0100

    migration to vite.

--- svelte-files-rollup/src/components/mainPageComponents/sections/Footer.svelte
+++
@@ -1,5 +0,0 @@
-<footer
-  class="bg-emerald-400 py-3 px-4 text-center font-semibold text-white transition-colors duration-500 dark:bg-indigo-800"
->
-  Made with Svelte + Tailwind.
-</footer>
--- svelte-files-rollup/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: ['./src/**/*.svelte'],
-  darkMode: 'class', // or 'media' or 'class'
-  theme: {
-    extend: {}
-  },
-  plugins: []
-}
