0f26123bdb5c92dd8451e13db709cc710ecb8fe9
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2023-12-12 19:27:44 +0000

    about me change.

--- svelte-files/src/components/mainPageComponents/sections/MidAboutMeSection.svelte
+++ svelte-files/src/components/mainPageComponents/sections/MidAboutMeSection.svelte
@@ -4,12 +4,12 @@
   <div class="mx-auto max-w-5xl px-6 text-center">
     <h2 class="text-3xl font-semibold text-white">About Me</h2>
     <p class="mt-4 text-gray-400">
-      I enjoy playing with frontend technologies, having worked with Typescript,
-      and frameworks such as React, Vue and Svelte. Im also skilled in cloud
-      development, building apps with Firebase, and developing professionally
-      with the AWS ecosystem, integrating services such as Lambda, Amplify, and
-      DynamoDB. I am someone who loves to learn, as well as teach, sharing new
-      techs I discover with the brilliant developer community.
+      I am someone who enjoys playing with different technologies; Typescript,
+      frameworks such as React and Svelte♡, cloud development, and much more. I
+      develop professionally with the AWS ecosystem, integrating services such
+      as Lambda, Amplify, and DynamoDB. I am someone who loves to learn, as well
+      as teach, sharing new techs I discover with the brilliant developer
+      community.
     </p>
   </div>
 </section>
