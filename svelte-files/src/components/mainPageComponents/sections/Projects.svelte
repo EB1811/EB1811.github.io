8035a49dce8f6bc99e9f2d56801c31732a43853a
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2024-01-22 18:19:56 +0000

    Added tools

--- svelte-files/src/components/mainPageComponents/sections/Projects.svelte
+++ svelte-files/src/components/mainPageComponents/sections/Projects.svelte
@@ -21,7 +21,7 @@
         <h2 class="text-center text-lg font-semibold text-white">
           Predira Ltd
         </h2>
-        <p class="mt- text-sm text-gray-300">
+        <p class="text-sm text-gray-300">
           SaaS platform allowing users to create and maintain a custom trading
           platform, finetuning the products, maturities, and negotiation
           process.
@@ -52,14 +52,49 @@
           imgAlt="Image of TaskScape website"
         />
         <ProjectLink
-          title="Quiz Creator"
-          desc="A website where users can create and complete quizzes."
-          tech="React + Firebase"
-          link="https://github.com/EB1811/SimpleQuizzes-React"
-          imgLink="img/home-page-quiz-app.PNG"
-          imgAlt="Image of Quiz Creator website"
+          title="Twitch Stream Finder"
+          desc="Chrome extension to find Twitch streams filtered by categories and viewer counts."
+          tech="Svelte + Typescript + Vite."
+          link="https://github.com/EB1811/TwitchDiscovererExtension"
+          imgLink="img/twitch-stream-finder.PNG"
+          imgAlt="Image of Twitch Stream Finder extension"
         />
       </div>
+    </div>
+    <!-- <hr class="mt-12 border-gray-500" /> -->
+    <h2 class="mt-10 text-2xl font-semibold text-white">Tools</h2>
+    <div class="mt-10">
+      <a
+        href="https://github.com/EB1811/twitch-to-clip?tab=readme-ov-file"
+        class="mx-auto block max-w-xs overflow-hidden rounded-md bg-gray-600 transition duration-500 ease-in-out hover:-translate-y-1 hover:scale-110 dark:bg-neutral-800 lg:max-w-lg"
+      >
+        <div class="px-3 py-2 text-center text-sm">
+          <div class="span relative">
+            <svg
+              class="absolute right-0 top-2 h-3 w-3 text-gray-400"
+              fill="currentColor"
+              version="1.1"
+              id="Layer_1"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              viewBox="0 0 122.6 122.88"
+              ><g
+                ><path
+                  d="M110.6,72.58c0-3.19,2.59-5.78,5.78-5.78c3.19,0,5.78,2.59,5.78,5.78v33.19c0,4.71-1.92,8.99-5.02,12.09 c-3.1,3.1-7.38,5.02-12.09,5.02H17.11c-4.71,0-8.99-1.92-12.09-5.02c-3.1-3.1-5.02-7.38-5.02-12.09V17.19 C0,12.48,1.92,8.2,5.02,5.1C8.12,2,12.4,0.08,17.11,0.08h32.98c3.19,0,5.78,2.59,5.78,5.78c0,3.19-2.59,5.78-5.78,5.78H17.11 c-1.52,0-2.9,0.63-3.91,1.63c-1.01,1.01-1.63,2.39-1.63,3.91v88.58c0,1.52,0.63,2.9,1.63,3.91c1.01,1.01,2.39,1.63,3.91,1.63h87.95 c1.52,0,2.9-0.63,3.91-1.63s1.63-2.39,1.63-3.91V72.58L110.6,72.58z M112.42,17.46L54.01,76.6c-2.23,2.27-5.89,2.3-8.16,0.07 c-2.27-2.23-2.3-5.89-0.07-8.16l56.16-56.87H78.56c-3.19,0-5.78-2.59-5.78-5.78c0-3.19,2.59-5.78,5.78-5.78h26.5 c5.12,0,11.72-0.87,15.65,3.1c2.48,2.51,1.93,22.52,1.61,34.11c-0.08,3-0.15,5.29-0.15,6.93c0,3.19-2.59,5.78-5.78,5.78 c-3.19,0-5.78-2.59-5.78-5.78c0-0.31,0.08-3.32,0.19-7.24C110.96,30.94,111.93,22.94,112.42,17.46L112.42,17.46z"
+                /></g
+              ></svg
+            >
+          </div>
+          <h2 class="text-center text-lg font-semibold text-white">
+            Twitch-To-Clip
+          </h2>
+          <p class="mt-1 text-gray-300">
+            CLI tool that allows you to download Twitch clips and convert them
+            into vertical videos ideal for TikTok and YouTube Shorts.
+          </p>
+          <span class="mt-2 block text-sm text-gray-400"> Python </span>
+        </div>
+      </a>
     </div>
     <div class="mt-8 flex items-center justify-center">
       <a
